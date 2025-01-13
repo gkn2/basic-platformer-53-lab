f306216cfe17801cf279a741485731c6e9bd749e
gkn2 <112503427+gkn2@users.noreply.github.com>
2025-01-13 11:41:55 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy = -250
+})
+function placeplayer () {
+    for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
+        mySprite = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . b 5 5 b . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . b b b b b 5 5 5 5 5 5 5 b . . 
+            . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+            . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+            . . b d 5 5 b 1 f f 5 4 4 c . . 
+            b b d b 5 5 5 d f b 4 4 4 4 b . 
+            b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+            c d d d c c b 5 5 5 5 5 5 5 b . 
+            c b d d d d d 5 5 5 5 5 5 5 b . 
+            . c d d d d d d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . . 
+            `, SpriteKind.Player)
+        controller.moveSprite(mySprite, 100, 0)
+        tiles.placeOnTile(mySprite, value)
+        tiles.setTileAt(value, assets.tile`transparency16`)
+        scene.cameraFollowSprite(mySprite)
+        mySprite.ay = 500
+    }
+}
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`level2`)
+placeplayer()
